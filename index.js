188feb9f84b995258eae3877845447631c989615
Constance <crsampson@gmail.com>
2023-04-12 22:30:20 -0400

    arraysLab

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-// Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendCat(name) {
-    cats.push(name) 
+    cats.push(name)
 }
 function destructivelyPrependCat(name) {
-    cats.unshift(name) 
+    cats.unshift(name)
 }
 function destructivelyRemoveLastCat() {
-    cats.pop()
+    cats.pop(name)
 }
 function destructivelyRemoveFirstCat() {
-    cats.shift()
+    cats.shift(name)
 }
 function appendCat(name) {
     return [...cats, name]
 }
 function prependCat(name) {
-    return[name, ...cats]
+    return [name, ...cats]
 }
-function removeLastCat() {
-    return cats.slice(0, cats.length - 1)
+function removeLastCat(name) {
+    return cats.slice (0, cats.length -1)
 }
-function removeFirstCat() {
-    return cats.slice(1, cats.lenth)
+function removeFirstCat(name) {
+    return cats.slice (1, cats.length)
 }
